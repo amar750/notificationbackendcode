dbfd6634bccbb79f6a8aeccdd0788b55dee7b0c3
amar750 <ak.dev1709@gmail.com>
2018-01-14 16:07:11 +0530

    added

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php 
+	define('DB_USERNAME','root');
+	define('DB_PASSWORD','amarguriya');
+	define('DB_NAME','fcm');
+	define('DB_HOST','localhost');
+
+	define('FIREBASE_API_KEY', 'AAAAaG2kWu4:APA91bG29RpcPgfhsrnU7yVK9vjNCjlxn9z7pfJdRnBfDlNfwQbTrUfyb41WARJl9lbSLOlhvc02OfxiTBjZfZOUe-3uE1hKrRkWphddk7k6z9we7J8pgFmuAHMLnsbliC1pGwenV4tC');
